c5cba8b70965cd95653f34d5f7996603c1204305
Tanisha Sharma <92216346+Tanishasharma11@users.noreply.github.com>
2023-07-12 15:03:47 +0530

    Create automatic-lamp

---
+++ automatic-lamp.ino
@@ -0,0 +1,20 @@
+int led1=13;
+void setup()
+{
+ Serial.begin(9600);
+ pinMode(led1,OUTPUT) ;
+}
+
+void loop()
+{
+  int c = analogRead(A0) ;
+  Serial.println(c) ;
+  if(c<800)
+  {
+    digitalWrite(led1,LOW) ; 
+  }
+  else
+  {
+    digitalWrite(led1,HIGH) ; 
+  }
+}
